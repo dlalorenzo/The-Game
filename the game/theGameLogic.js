b48747c3ef08ba29a0b03f0e5bb7fcd3ece0dd1b
lorenzo <99051451@mydavinci.nl>
2018-09-25 08:35:19 +0200

    https://github.com/dlalorenzo/The-Game

---
+++ the game/theGameLogic.js
@@ -0,0 +1,169 @@
+if (prompt("welcome press go to start the heist if you survived you won if died you lose","go")==="go"){
+ alert("you wake up in your house at 10:30 m")
+
+  if(prompt("what you gone do next sleep for some more hours or go outside","sleep-outside")==="sleep"){
+    alert("you have slept for 2 more hours its now 12:30")
+
+    if (prompt("do you wanna go eat or take a shower","eat-shower")==="eat"){
+      alert("you have gone downstairs and you see that the frigde is empty zo you gone order somthing")
+
+      if(prompt("do want some pizza or kapsalon","pizza-kapsalon")==="pizza"){
+        alert("you have orderd a pizza")
+         alert("you have taken a shower and you pizza is done");
+         alert = function(end) {};
+
+      }else
+      alert("you have orderd a kapsalon")
+      alert("you have taken a shower and you kapsalon is done")
+      alert("you gone go outside do you want a car or a motor")
+      if(prompt("a car or a motor","car-motor")==="car"){
+      alert("you have choosen the car") 
+      alert("witch car do you want")
+        if(prompt("corvette or a ford mustang","ford-corvette")==="ford"){
+          alert("you are going to the highway")
+           if(prompt("are you going to drive to hard or the normal speed limit","hard-normal")=== "hard"){
+            alert("you drove to hard and you crashed and you died 2 hours later in the hospital")
+             alert("game over"); 
+
+
+           }else
+            alert("you drove to a meeting where all the people where for the heist")
+             alert ("what roll do you wanna play?")
+              if (prompt("geteway or robber")==="robber"){
+                alert("you choose to be a robber")
+                 alert("what wappen do you want ")
+                 if(prompt("a assault or a shotgun","assault-shotgun")==="assault"){
+
+                 }else
+                  alert("you choose the shotgun")
+                   alert ("you arrived at the bank")
+                    alert("what is your next roll")
+                     if (prompt("go to the storage or keep the gaurd","storage-gaurd")==="gaurd"){
+                      alert("you are the gaurd")
+                       alert("the police came in and arrested you")
+                        alert("game over")
+                          alert = function(end) {};
+                     }else
+                      alert("your the one who is going too the storage")
+                       alert("whole team had been arresten only you are left what you gone do ?")
+                        if (prompt("go too the geteway car or get arrested too?","geteway-arrested")==="geteway"){
+                          alert ("you made it too the geteway and got away with the money")
+                           alert ("you won")
+                            alert = function(end) {};
+               }else
+                alert("you gave your self over and got arrested")
+                 alert("game over") 
+                 alert = function(end) {};
+
+              }else
+               alert("you are the geteway driver")
+                alert("while you where waiting outside you were shot and died instant in your car")
+                 alert("game over")
+
+        }else
+        alert("you have choosen the corvette")
+         alert("you where on your way too the heist meeting and the corvette broke down")
+          if(prompt("do you wanna take a bus home or too the meeting","home-meeting")==="home"){
+            alert("you are now safe home")
+             alert("5 hours later:the hole heist went wrong but you are safe home")
+              alert("you won")
+          }else
+          alert("you went with the bus to the meeting spot")
+           alert("but then you arrived you see that you are on the wrong spot")
+            if (prompt("are you gone run or give over")==="run"){
+              alert("you are going to run")
+               alert("while you were triyng to get away you got shot and they let you bleed to dead")
+                alert("game over")
+            }else
+             alert("you gave over and they took you with them en killed you because you already knowed to mutch")
+              alert("game over")
+
+
+      }else
+      alert("you choosen the motor")
+      alert("witch motor do you want")
+      if (prompt("a kawasaki or a harley-davidson","kawasaki-harley")==="kawasaki"){
+       alert("you choose the kawasaki")
+        alert("the kawasaki was stolen and 10 minutes later on your way the politicie caught you and arrested you")
+         alert("game over")
+      }
+    }else
+    alert("you have choosen the harley-davidson")
+     alert("you went for a drive")
+    if(prompt("where do you wanna go beach or highway?","beach-highway")==="beach"){
+      alert("you are driving on the beach and the gaurd send you away so went to get som food")
+       alert("what do you want to eat")
+        if(prompt("macdonald or kfc?")==="macdonald"){
+          alert("you orderd some and went home too eat it")
+           alert("the day after you found a gold coint that had over 1 mil valau")
+            alert("you sold it for 1,5mill")
+             alert("you bought al kinds of thing new house new bike and lots more")
+              alert("you won")
+
+        }else
+         alert("you went to the kfc and went home too eat it")
+          alert("but its was not good its was a wrong chicken and you got sick")
+           alert("its was so bad you died 2 weeks later")
+            alert("game over")
+
+
+    }else
+    if (alert("you where driving on the highway and crashed your motor under a truck that stopped driving you died"))
+     alert("game over")
+  }else
+   alert("you have taken a shower and went outside you wanna go for a ride ")
+  if (prompt("what do you pick to drive a motor or a car","motor-car")==="car"){
+
+    alert("you choose the car witch car do you want?")
+    if(prompt("toyota supra or nissan skyline","supra-skyline")==="supra"){
+      alert("you have choosen the toyota supra")
+       alert("you are on your way to a heist")
+        alert("you get there what rol do you wanna get")
+         if(prompt("the geteway driver or the robber","driver-robber")==="geteway"){
+          alert("you arrived at the bank everting inside went good and you guys where gone in 10 minutes")
+           alert("you won")
+         }else
+          alert("you are the robber")
+           alert("what wappen do you choose")
+            if (prompt("a pistol or a minigun","pistol-minigun")==="minigun"){
+              alert("you choose the minigun")
+               alert("the minigun was to heavy to run with and you died")
+                alert("game over")
+
+            }else
+             alert("you are inside")
+              alert("whar roll do you want now")
+               if (prompt("the gaurd or going to the storage","gaurd-storage")==="storage"){
+                alert("there was a time bomb on the storage and you could not de activate it you died")
+                 alert("game over")
+
+               }else
+                alert("while you where on the gaurd you heard the safe explode and you saw your dead friend so")
+                 alert("you runned away to the get away with a little bit money you picked up fast")
+                  alert("you won")
+
+    }else
+    alert("you have choosen the skyline")
+     alert("where do you wanna go too ?")
+     if (prompt("the mall or the garage","mall-garage")==="mall"){
+      alert("you are going to the mall")
+       alert("when you get there you got arrested for soms old crimes you did")
+        alert("game over")
+     }else
+      alert("you are going to the garage ")
+       alert("you have done som repairs and upgrades on your car")
+        alert("the upgrades on your care where not good and the car burned up in flames with you in it")
+         alert("game over")
+
+  }else
+   alert("you choose a motor witch motor you want")
+    if(prompt("bmw or a honda","bmw-honda")==="bmw"){
+      alert("you have choosen the bmw")
+       alert(" ")
+
+    }else 
+    alert("you have choosen the honda")
+    if(prompt("")){
+
+    }
+}
